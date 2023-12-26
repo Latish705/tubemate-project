359cec64e13d6d42dc2f88d7421d77b3729d55f2
Latish Adwani <104522651+Latish705@users.noreply.github.com>
2023-12-26 09:53:47 +0530

    working on login and logout backend

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,7 +1,18 @@
 import { User } from "../models/userModel.js";
 import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import uploadToCloudinary from "../utils/cloudinary.js";
+
+const generateAccessandRefreshToken = async (userId) => {
+  const user = User.findOne({ userId });
+  const accessToken = user.generateAccessToken();
+  const refreshToken = user.generateRefreshToken();
+  user.refreshToken = refreshToken;
+  await user.save({ validateBeforeSave: false });
+
+  return { accessToken, refreshToken };
+};
 
 export const registerUser = asyncHandler(async (req, res) => {
   //first we will check the all the inputs are present in request
@@ -28,4 +39,44 @@ export const registerUser = asyncHandler(async (req, res) => {
 
   const avatar = await uploadToCloudinary(avatarLocalPath);
   console.log(avatar.url);
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, username, password } = req.body;
+  if ([email, username, password].some((fields) => fields.trim() === "")) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const user = await User.findOne({
+    $or: [{ email }, { username }],
+  });
+
+  if (!user) {
+    throw new ApiError(400, "Unable to find user");
+  }
+
+  const passwordMatch = await user.isPasswordCorrect(password);
+
+  if (!passwordMatch) {
+    throw new ApiError(200, "Password incorrect");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessandRefreshToken(
+    user._id
+  );
+  
+  const loggedInUser = await User.findById({user._id}).select("-password refreshToken");
+
+  const options = {
+    httpOnly : true,
+    secure : true,
+  }
+
+  return res
+    .status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(
+      new ApiResponse(200,{user:loggedInUser,accessToken,refreshToken},"User loggen in successfully")
+    )
 });
--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userScheme = mongoose.Schema({
   username: {
@@ -44,5 +46,42 @@ const userScheme = mongoose.Schema({
     type: String,
   },
 });
+
+userScheme.pre("save", async function (next) {
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userScheme.methods.isPasswordCorrect = async (password) => {
+  return await bcrypt.compare(password, this.password);
+};
+
+userScheme.methods.generateAccessToken = async () => {
+  return jwt.sign(
+    {
+      id: this._id,
+      username: this.username,
+      email: this.email,
+      password: this.password,
+      fullName: this.fullName,
+    },
+    process.env.JWT_ACCESS_TOKEN,
+    {
+      expiresIn: process.env.JWT_ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userScheme.methods.generateRefreshToken = async () => {
+  return jwt.sign(
+    {
+      id: this._id,
+    },
+    process.env.JWT_REFRESH_TOKEN,
+    {
+      expiresIn: process.env.JWT_REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userScheme);
