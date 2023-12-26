fd547c74f4af497a3cdc278d12721ab3c9c5a8d2
Latish Adwani <104522651+Latish705@users.noreply.github.com>
2023-12-27 02:29:53 +0530

    Add initial project files

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+function App() {
+  return (
+    <>
+      <h1 className="bg-red-400">hello</h1>
+      <Navbar></Navbar>
+    </>
+  );
+}
+
+export default App;
